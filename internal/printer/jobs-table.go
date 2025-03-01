e598a2b4976081c52be7fc24e13d61212a9baa9d
Edwin Ade <edwinmbonyjr@gmail.com>
2025-03-01 08:36:17 +0100

    fix(internal/printer/table): remove unnecessary comments

--- internal/printer/jobs-table.go
+++ internal/printer/jobs-table.go
@@ -8,16 +8,10 @@ import (
 	"github.com/valentino7504/jobtrack/internal/db"
 )
 
-// location, salary_range, job posting
 func PrintJobsTable(jobs []*db.Job) {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.Debug|tabwriter.AlignRight)
 	fmt.Fprintf(w, "ID\tCompany\tPosition\tStatus\tLocation\tSalary Range\tApplied On\n")
 	for _, job := range jobs {
-		// location := "N/A"
-		// fmt.Println(job.Location.Valid)
-		// if job.Location.Valid {
-		// 	location = job.Location.String
-		// }
 		fmt.Fprintf(
 			w,
 			"%d\t%s\t%s\t%s\t%s\t%s\t%s\n",
