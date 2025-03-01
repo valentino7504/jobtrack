c4f754a37cb8540a1dcf03bd0d9da7910edc296c
Edwin Ade <edwinmbonyjr@gmail.com>
2025-03-01 18:16:34 +0100

    feat(cmd): implement list command to list jobs by id, status and application date

---
+++ cmd/list.go
@@ -0,0 +1,86 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/valentino7504/jobtrack/internal/db"
+	"github.com/valentino7504/jobtrack/internal/printer"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List job applications with optional filters",
+	Long: `Lists all job applications stored in the database by default.
+	You can filter results using the following flags:
+	- "--id <job_id>" -> Fetches a job by ID (e.g. jobtrack list --id 3
+	- "--status <job_status>" -> Fetches jobs by status`,
+	Run: func(cmd *cobra.Command, args []string) {
+		jobID, _ := cmd.Flags().GetInt("id")
+		status, _ := cmd.Flags().GetString("status")
+		after, _ := cmd.Flags().GetString("after")
+		before, _ := cmd.Flags().GetString("before")
+		defaultBefore := "1970-01-01"
+		defaultAfter := db.FormatDateTime(time.Now(), true)
+		caser := cases.Title(language.English)
+		status = caser.String(status)
+
+		switch {
+		case jobID > -1:
+			job, err := db.GetJobByID(SqliteDB, jobID)
+			if err != nil {
+				fmt.Println("Error getting job:", err)
+				return
+			}
+			if job == nil {
+				fmt.Println("No job found with ID:", jobID)
+				return
+			}
+			printer.PrintJob(job)
+		case status != "":
+			jobs, err := db.GetJobsByStatus(SqliteDB, db.JobStatus(status))
+			if err != nil {
+				fmt.Println("Error getting jobs:", err)
+				return
+			}
+			if len(jobs) == 0 {
+				fmt.Println("No jobs found with that status")
+				return
+			}
+			printer.PrintJobsTable(jobs)
+		case after != defaultAfter || before != defaultBefore:
+			jobs, err := db.GetJobsByDate(SqliteDB, before, after)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if len(jobs) == 0 {
+				fmt.Println("No jobs found within the specified date range")
+				return
+			}
+			printer.PrintJobsTable(jobs)
+		default:
+			jobs, err := db.GetAllJobs(SqliteDB)
+			if err != nil {
+				fmt.Println("Error getting jobs", err)
+				return
+			}
+			if len(jobs) == 0 {
+				fmt.Println("No job applications available")
+				return
+			}
+			printer.PrintJobsTable(jobs)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+	listCmd.Flags().Int("id", -1, "The integer index of the job")
+	listCmd.Flags().String("status", "", "The status of the job")
+	listCmd.Flags().String("after", "1970-01-01", "List jobs applied on or after this date")
+	listCmd.Flags().String("before", db.FormatDateTime(time.Now(), true), "List jobs applied on or before this date")
+}
