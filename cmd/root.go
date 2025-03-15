19dd2c024e52b15ab400333df017ecfb9c01daae
Edwin Ade <edwinmbonyjr@gmail.com>
2025-03-16 00:25:20 +0100

    feat(root): add version tag

--- cmd/root.go
+++ cmd/root.go
@@ -12,12 +12,11 @@ var SqliteDB *sql.DB
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "jobtrack",
-	Short: "A CLI tool built in Go to track job applications efficiently.",
+	Use:     "jobtrack",
+	Short:   "A CLI tool built in Go to track job applications efficiently.",
+	Version: "1.0.0",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -26,18 +25,10 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jobtrack.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.CompletionOptions.HiddenDefaultCmd = true
 }
 
-// Sets the application db to the Sqlite DB handle.
 func SetDB(dbHandle *sql.DB) {
 	SqliteDB = dbHandle
 }
